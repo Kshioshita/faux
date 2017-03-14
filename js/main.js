671226755da7709e42b19271bb851635df6dd36e
Kaylin Shioshita <kaylin.shioshita@colorado.edu>
2017-03-14 15:12:49 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var quoteId=['quote1', 'quote2', 'quote3']
+var quoteNumber=0;
+var maxQuotes=3;
+window.setInterval(changeQuote, 10000);
+
+function changeQuote(){
+	document.getElementById(quoteId[quoteNumber]).classList.remove('active');
+	document.getElementById(quoteId[quoteNumber]).classList.add('hidden');
+	quoteNumber++;
+	if(quoteNumber>maxQuotes-1){
+		quoteNumber=0;
+	}
+	document.getElementById(quoteId[quoteNumber]).classList.remove('hidden');
+	document.getElementById(quoteId[quoteNumber]).classList.add('active');
+
+
+	// document.getElementById(quoteId[quoteNumber]).style.display = "none";
+	// quoteNumber++;
+	// if(quoteNumber>maxQuotes-1){
+	// 	quoteNumber=0;
+	// }
+	// document.getElementById(quoteId[quoteNumber]).style.display = "block";
+}
